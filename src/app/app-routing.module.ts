53ef65bcd10dff8c61cacbe8ceaeb722cf9076be
Francisco-Novoa <61065793+Francisco-Novoa@users.noreply.github.com>
2022-10-25 10:55:22 -0300

    removing extra 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthService } from './services/auth.service';
-
 const routes: Routes = [
   // Estos cargan la pagina
   {
